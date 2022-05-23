40d2d40f4bd33ecfe8fdd76da4039222d414833b
Stuvers <johnstuvers@gmail.com>
2022-05-23 22:49:08 +0100

    Added classes to javascript and messed with the css
    Added classes to javascript and messed with the css

--- js/xylophoneControls.js
+++ js/xylophoneControls.js
@@ -1,7 +1,43 @@
 // @Stuvers #task 
 var isRecording = false;
+var notes = [];
 var songList = [];
 var noteList = [];
+
+//note class
+class Note {
+	constructor(id, note, audio, alternativeAudio) {
+		this.id = id;
+		this.note = note;
+		this.audio = audio;
+		this.alternativeAudio = alternativeAudio;
+	}
+
+	playAudio() {
+		let sound = new Audio(this.audio);
+		sound.play();
+	}
+}
+
+function createNotes()
+{
+	// C1 note
+	notes[0] = new Note(1, "C1", "music\\xylophone-c.wav", "music\\xylophone-c.wav");
+	// D2 note
+	notes[1] = new Note(2, "D2", "music\\xylophone-d1.wav", "music\\xylophone-d1.wav");
+	// E3 note
+	notes[2] = new Note(3, "E3", "music\\xylophone-e1.wav", "music\\xylophone-e1.wav");
+	// F4 note
+	notes[3] = new Note(4, "F4", "music\\xylophone-f.wav", "music\\xylophone-f.wav");
+	// G5 note
+	notes[4] = new Note(5, "G5", "music\\xylophone-g.wav", "music\\xylophone-g.wav");
+	// A6 note
+	notes[5] = new Note(6, "A6", "music\\xylophone-a.wav", "music\\xylophone-a.wav");
+	// B7 note
+	notes[6] = new Note(7, "B7", "music\\xylophone-b.wav", "music\\xylophone-b.wav");
+	// C8 note
+	notes[7] = new Note(8, "C8", "music\\xylophone-c2.wav", "music\\xylophone-c2.wav");	
+}
 
 // This will eventually be the max allowed songs and notes
 // These will be the limits on the site 
@@ -9,13 +45,13 @@ const maxSongs = 9;
 const maxNotes = 9;
 
 function toggleRecording() {
-	let recording = document.getElementById("recording");
+	let record = document.getElementById("record");
 	let songs = document.getElementById("songs")
 
 	if (!isRecording)
 	{
 		isRecording = true;
-		recording.innerHTML = "Stop Recording!";
+		record.innerHTML = "Stop Recording!";
 	}
 	else
 	{
@@ -30,13 +66,11 @@ function toggleRecording() {
 		}
 	    
 		songs.innerHTML = content;
-		recording.innerHTML = "Start Recording!";
+		record.innerHTML = "Start Recording!";
 	}
 }
 
 function HitBar(note) {
-	let audio;
-
 	if (noteList.length < maxNotes && isRecording)
 	{
 		noteList.push(note);
@@ -49,36 +83,14 @@ function HitBar(note) {
 		console.log("=10 " + noteList.length);
 	}
 
-	if (note == "C1") {
-		audio = new Audio("music\\xylophone-c.wav");
-		audio.play();
+	playNote(note);
+}
+
+function playNote(note) {
+	for (let curNote of notes) {
+		if (curNote.note === note)
+		{
+			curNote.playAudio();
+		}
 	}
-	else if (note == "D2") {
-		audio = new Audio("music\\xylophone-d1.wav");
-		audio.play();
-	}
-	else if (note == "E3") {
-		audio = new Audio("music\\xylophone-e1.wav");
-		audio.play();
-	}
-	else if (note == "F4") {
-		audio = new Audio("music\\xylophone-f.wav");
-		audio.play();
-	}
-	else if (note == "G5") {
-		audio = new Audio("music\\xylophone-g.wav");
-		audio.play();
-	}
-	else if (note == "A6") {
-		audio = new Audio("music\\xylophone-a.wav");
-		audio.play();
-	}
-	else if (note == "B7") {
-		audio = new Audio("music\\xylophone-b.wav");
-		audio.play();
-	}
-	else if (note == "C8") {
-		audio = new Audio("music\\xylophone-c2.wav");
-		audio.play();
-	}	
 }
